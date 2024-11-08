6a059b734d89668770119f0ef42f0d7b7d9b2497
carrot2803 <lezamadmitri@gmail.com>
2024-11-08 12:37:05 -0400

    update config loader

--- src/main/java/com/gophers/services/helpers/GradeConfigLoader.java
+++ src/main/java/com/gophers/services/helpers/GradeConfigLoader.java
@@ -1,50 +1,20 @@
 package com.gophers.services.helpers;
 
+import com.gophers.interfaces.ConfigLoaderStrategy;
 import com.gophers.structures.domain.TestFeedback;
-import org.yaml.snakeyaml.Yaml;
 
-import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
 public class GradeConfigLoader {
-	private static final Map<String, Map<String, Integer>> weightings = new HashMap<>();
-	private static final Map<String, Map<String, TestFeedback>> feedback = new HashMap<>();
+	private static final Map<String, Map<String, Integer>> weightings;
+	private static final Map<String, Map<String, TestFeedback>> feedback;
+
+	private static final ConfigLoaderStrategy configLoader = new ConfigLoader();
+
 	static {
-		loadConfigurations();
-	}
-
-	@SuppressWarnings("unchecked")
-	private static void loadConfigurations() {
-		Yaml yaml = new Yaml();
-		try (InputStream inputStream = GradeConfigLoader.class.getClassLoader().getResourceAsStream("data.yaml")) {
-			if (inputStream == null) {
-				throw new RuntimeException("YAML configuration file not found.");
-			}
-			Map<String, Object> config = yaml.load(inputStream);
-
-			Map<String, Map<String, Integer>> weightingsConfig = (Map<String, Map<String, Integer>>) config
-					.get("weightings");
-			for (Map.Entry<String, Map<String, Integer>> entry : weightingsConfig.entrySet()) {
-				weightings.put(entry.getKey(), entry.getValue());
-			}
-
-			Map<String, Map<String, Object>> feedbackConfig = (Map<String, Map<String, Object>>) config
-					.get("testFeedback");
-			for (Map.Entry<String, Map<String, Object>> entry : feedbackConfig.entrySet()) {
-				Map<String, TestFeedback> feedbackMap = new HashMap<>();
-				for (Map.Entry<String, Object> feedbackEntry : entry.getValue().entrySet()) {
-					Map<String, Object> feedbackData = (Map<String, Object>) feedbackEntry.getValue();
-					String message = (String) feedbackData.get("feedback");
-					int score = (int) feedbackData.get("priority");
-					feedbackMap.put(feedbackEntry.getKey(), new TestFeedback(message, score));
-				}
-				feedback.put(entry.getKey(), feedbackMap);
-			}
-
-		} catch (Exception e) {
-			throw new RuntimeException("Error loading configuration from YAML", e);
-		}
+		weightings = configLoader.loadWeightings();
+		feedback = configLoader.loadFeedback();
 	}
 
 	public static Map<String, Integer> getWeightings(String gradeType) {
