6f8cecf3056d57bfb04f31717d2f1ab77b441297
meiying13 <FeliciaMeiying13@gmail.com>
2024-11-10 17:41:39 -0400

    added Facade performance test

---
+++ src/test/java/com/gophers/FacadePerformanceTest.java
@@ -0,0 +1,29 @@
+package com.gophers;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.gophers.interfaces.Facade;
+import com.gophers.services.handlers.GradingFacade;
+
+public class FacadePerformanceTest {
+
+    private static final PrintStream originalOut = System.out;
+    private final long performanceThreshold = 15000;
+
+    @Test
+    public void testFacadePerformance() {
+        System.setOut(new PrintStream(OutputStream.nullOutputStream())); // hiding output from processSubmissions
+        Facade facade = new GradingFacade();
+        long startTime = System.nanoTime();
+        facade.processSubmissions(null);
+        long endTime = System.nanoTime();
+        System.setOut(originalOut);
+        long duration = (endTime - startTime) / 1_000_000;
+        System.out.println("Execution Time for Facade: " + duration + " ms");
+        Assert.assertTrue("Test execution took too long: " + duration + " ms", duration < performanceThreshold);
+    }
+}
