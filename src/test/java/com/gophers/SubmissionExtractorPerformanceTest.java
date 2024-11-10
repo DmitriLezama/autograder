b2d2222361506245ce973faa047735074d307857
meiying13 <FeliciaMeiying13@gmail.com>
2024-11-10 18:18:06 -0400

    added SubmissionExtractor performance test

---
+++ src/test/java/com/gophers/SubmissionExtractorPerformanceTest.java
@@ -0,0 +1,26 @@
+package com.gophers;
+
+import org.junit.Test;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import com.gophers.services.handlers.SubmissionExtractor;
+
+public class SubmissionExtractorPerformanceTest {
+    
+    private final long performanceThreshold = 2000;
+
+    @Test
+    public void testSubmissionExtractorPerformance() {
+        long startTime = System.nanoTime();
+        List<String> submissions = SubmissionExtractor.extractSubmissions("submissions.zip");
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime) / 1_000_000;
+        System.out.println("Execution Time for Submission Extractor: " + duration + " ms");
+        assertNotNull(submissions);
+        assertTrue("Test execution took too long: " + duration + " ms", duration < performanceThreshold);
+    }
+
+}
