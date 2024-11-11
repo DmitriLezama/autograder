0f65f9dd11c8ca14bbac1fb8c9884901dda1249c
ando <ando6703@gmail.com>
2024-11-10 22:52:55 -0400

    added grade factory unit test

---
+++ src/test/java/com/gophers/unit/GradeFactoryUnitTest.java
@@ -0,0 +1,37 @@
+package com.gophers.unit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.Result;
+
+import com.gophers.services.handlers.AssignmentTestRunner;
+import com.gophers.structures.factory.GradeFactory;
+import com.gophers.interfaces.Grade;
+
+public class GradeFactoryUnitTest {
+
+    @Test
+    public void TestCreateGrade(){
+        List<Grade> grades = new ArrayList<Grade>();
+        GradeFactory factory = new GradeFactory();
+        List<Result> results = new AssignmentTestRunner().runAllTests();
+        for(int i = 0; i < results.size(); i++)
+            grades.add(factory.createGrade(results.get(i), i));
+        for(Grade g : grades)
+            Assert.assertTrue(g != null);      
+    }
+    
+
+    @Test
+    public void TestCreateGrades(){
+        GradeFactory factory = new GradeFactory();
+        List<Result> results = new AssignmentTestRunner().runAllTests();
+        List<Grade> grades = factory.createGrades(results);
+        for(Grade g : grades)
+          Assert.assertTrue(g != null);  
+    }
+    
+}
