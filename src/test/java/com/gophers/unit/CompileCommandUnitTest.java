f992a0e0429867db941e6735d0e8b2dc9a6b0e59
carrot2803 <lezamadmitri@gmail.com>
2024-11-10 23:01:04 -0400

    deleted bad tests

--- src/test/java/com/gophers/unit/CompileCommandUnitTest.java
+++ src/test/java/com/gophers/unit/CompileCommandUnitTest.java
@@ -7,36 +7,26 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class CompileCommandUnitTest {
-
     @Test
-    public void testCompileWithValidJavaFile(){
-        String filePath = "src\\main\\resources\\studentSubmissions\\Brandon_Chandoo_816034693_A1";
+    public void testCompileWithValidJavaFile() {
+        String filePath = "src/main/resources/studentSubmissions/Brandon_Chandoo_816034693_A1";
         CompileCommand compiler = new JavaCompileCommand(filePath);
         boolean result = compiler.compile();
-        assertTrue(result);
+        assertTrue("Compilation failed", result);
     }
 
     @Test
-    public void testCompileWithInvalidJavaFile(){
-        String filePath = "src\\main\\resources\\studentSubmissions\\John_Doe_816123456_A1";
+    public void testCompileWithInvalidJavaFile() {
+        String filePath = "src/main/resources/studentSubmissions/John_Doe_816123456_A1";
         CompileCommand compiler = new JavaCompileCommand(filePath);
         boolean result = compiler.compile();
-        assertFalse(result);
-    }
-
-    @Test
-    public void testCompileWithNoJavaFiles() {
-        String filePath = "src\\main\\resources\\studentSubmissions\\Empty_Submission_816036111_A1";
-        CompileCommand compiler = new JavaCompileCommand(filePath);
-        boolean result = compiler.compile();
-        assertFalse(result);
+        assertFalse("Compilation succeeded", result);
     }
 
     @Test
     public void testCompileWithNonExistentDirectory() {
         CompileCommand compiler = new JavaCompileCommand("nonExistentDirectory");
         boolean result = compiler.compile();
-        assertFalse(result);
+        assertFalse("Compilation succeeded", result);
     }
 }
-
