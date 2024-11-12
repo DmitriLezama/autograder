03fe89401397aa334759e080fa4b5fcfb000a998
arashi009 <brandonchandoo66@gmail.com>
2024-11-12 01:43:50 -0400

    snakeyaml dependency removed

--- src/main/java/com/gophers/services/helpers/MarkSchemeLoader.java
+++ src/main/java/com/gophers/services/helpers/MarkSchemeLoader.java
@@ -2,20 +2,20 @@ package com.gophers.services.helpers;
 
 import com.gophers.interfaces.MarkSchemeLoaderStrategy;
 import com.gophers.utilities.Constants;
-import org.yaml.snakeyaml.Yaml;
+import com.gophers.utilities.Yaml;
+
 import java.io.InputStream;
 import java.util.Map;
 
 public class MarkSchemeLoader implements MarkSchemeLoaderStrategy {
-    @Override
     public Map<String, Map<String, Integer>> loadWeightings() {
         try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(Constants.MARK_SCHEME)) {
             if (inputStream == null) {
-                throw new RuntimeException("YAML configuration file not found.");
+                throw new IllegalStateException("Could not find mark scheme resource: " + Constants.MARK_SCHEME);
             }
-            return new Yaml().loadAs(inputStream, Map.class);
+            return new Yaml().loadAs(inputStream);
         } catch (Exception e) {
-            throw new RuntimeException("Error loading weightings from YAML", e);
+            throw new IllegalStateException("Failed to load mark scheme weightings", e);
         }
     }
 }
