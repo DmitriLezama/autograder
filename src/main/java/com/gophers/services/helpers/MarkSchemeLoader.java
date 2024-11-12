67fa6608dacb2c14903ca488f19a8c76a1ed9fc8
carrot2803 <lezamadmitri@gmail.com>
2024-11-12 00:45:27 -0400

    fixed markscheme loader

--- src/main/java/com/gophers/services/helpers/MarkSchemeLoader.java
+++ src/main/java/com/gophers/services/helpers/MarkSchemeLoader.java
@@ -4,20 +4,16 @@ import com.gophers.interfaces.MarkSchemeLoaderStrategy;
 import com.gophers.utilities.Constants;
 import org.yaml.snakeyaml.Yaml;
 import java.io.InputStream;
-import java.util.HashMap;
 import java.util.Map;
 
 public class MarkSchemeLoader implements MarkSchemeLoaderStrategy {
     @Override
-    @SuppressWarnings("unchecked")
     public Map<String, Map<String, Integer>> loadWeightings() {
-        Yaml yaml = new Yaml();
         try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(Constants.MARK_SCHEME)) {
             if (inputStream == null) {
                 throw new RuntimeException("YAML configuration file not found.");
             }
-            Map<String, Object> config = yaml.load(inputStream);
-            return (Map<String, Map<String, Integer>>) config.getOrDefault("weightings", new HashMap<>());
+            return new Yaml().loadAs(inputStream, Map.class);
         } catch (Exception e) {
             throw new RuntimeException("Error loading weightings from YAML", e);
         }
